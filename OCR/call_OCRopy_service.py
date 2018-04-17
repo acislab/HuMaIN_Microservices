f2077a5b0bace1184d1acd10764626e89a2f191f
jingchaoluan <jingchaoluan@gmail.com>
2018-04-17 13:05:33 -0400

    Update client to call OCRopus service

--- OCR/call_OCRopy_service.py
+++ OCR/call_OCRopy_service.py
@@ -21,19 +21,21 @@
 
 import requests
 import time, argparse, os
+import multiprocessing as mp
 
-start_time = time.time()
 
 IP = "10.5.146.92"
 PORT = "8000"
 
-LICHENS = "/home/jingchao/HuMaIN/label-data-master/lichens/gold/inputs" # 200 images
+UFHerbarium = "/home/jingchao/Research/UFHerbariumData/UFHerbarium_3000"
+LICHENS = "/home/jingchao/HuMaIN/label-data-master/lichens/gold/input_10" # 200 images
 HERB = "/home/jingchao/HuMaIN/label-data-master/herb/gold/inputs" # 100 images
 ENT = "/home/jingchao/HuMaIN/label-data-master/ent/gold/inputs" # 100 images
 
 
 ### Call OCRopy service
-def call_ocropy(imagepath, dstDir):
+def call_ocropy(job):
+	imagepath, dstDir = job
 	url_ocr = "http://" + IP + ":" + PORT + "/ocrapi"
 
 	# Uploaded iamges
@@ -57,22 +59,23 @@ def call_ocropy(imagepath, dstDir):
         return
 
 if __name__ == '__main__':
+	start_time = time.time()
+	jobs = []
 	for img in os.listdir(LICHENS):
 		img_path = os.path.join(LICHENS, img)
-		try:
-			call_ocropy(img_path, LICHENS)
-		except:
-			pass
+		call_ocropy((img_path, LICHENS)) # single process
+		#jobs.append((img_path, LICHENS))
+	#pool = mp.Pool(processes=6)
+	#pool.map(call_ocropy, jobs)
+	# Close processes pool after it is finished
+	#pool.close()
+	#pool.join()
 	lichens_end = time.time()
-	
-	for img in os.listdir(HERB):
-		img_path = os.path.join(HERB, img)
-		try:
-			call_ocropy(img_path, HERB)
-		except:
-			pass
-	herb_end = time.time()
+	print("\n*** 10 LICHENS images time cost: %.2f seconds ***\n" % (lichens_end - start_time))
+	#with open("OCRopyS_time.txt", 'ab+') as fd:
+	#	fd.write("\n*** 3000 UFHerbarium images' time cost: %.2f seconds ***\n" % (lichens_end - start_time))
 
+	"""
 	for img in os.listdir(ENT):
 		img_path = os.path.join(ENT, img)
 		try:
@@ -80,12 +83,20 @@ if __name__ == '__main__':
 		except:
 			pass
 	ent_end = time.time()
+	print("\n*** 100 herb images' time cost: %.2f seconds ***\n" % (ent_end - lichens_end))
 
-	with open("400iDigBio_OCRopyS_time.txt", 'wb') as fd:
+	for img in os.listdir(HERB):
+		img_path = os.path.join(HERB, img)
+		try:
+			call_ocropy(img_path, HERB)
+		except:
+			pass
+	herb_end = time.time()
+	print("\n*** 100 ent images' time cost: %.2f seconds ***\n" % (herb_end - ent_end))
+	
+	with open("400iDigBio_OCRopyS_time.txt", 'ab+') as fd:
 		fd.write("\n*** 200 lichens images' time cost: %.2f seconds ***\n" % (lichens_end - start_time))
-		fd.write("\n*** 100 herb images' time cost: %.2f seconds ***\n" % (herb_end - lichens_end))
-		fd.write("\n*** 100 ent images' time cost: %.2f seconds ***\n" % (ent_end - herb_end))
-
-	print("\n*** 200 lichens images' time cost: %.2f seconds ***\n" % (lichens_end - start_time))
-	print("\n*** 100 herb images' time cost: %.2f seconds ***\n" % (herb_end - lichens_end))
-	print("\n*** 100 ent images' time cost: %.2f seconds ***\n" % (ent_end - herb_end))
+		fd.write("\n*** 100 ent images' time cost: %.2f seconds ***\n" % (ent_end - lichens_end))
+		fd.write("\n*** 100 herb images' time cost: %.2f seconds ***\n" % (herb_end - ent_end))
+		fd.close()
+	"""
